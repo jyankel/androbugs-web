2f305ee592dd14d956970fbb0116b9f040db5da7
Joe Yankel <jdyankel@cert.org>
2018-12-14 15:38:10 -0500

    Added file upload to process single APK file

--- androbugs_web.py
+++ androbugs_web.py
@@ -1,18 +1,10 @@
 from flask import Flask
-from flask_debugtoolbar import DebugToolbarExtension
+#from flask_debugtoolbar import DebugToolbarExtension
 from models import db
-from views import index, detail, vector, update, delete
+from views import index, detail, vector, update, delete, upload
 
 
-def create_app():
-    '''
-    Bootstrap the app with actual Bootstrap css
-    :return: the flask application
-    '''
-    app = Flask(__name__)
-    return app
-
-app = create_app()
+app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {'DB': 'AndroBugs_DBs'}
 app.config['TESTING'] = True
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
@@ -21,14 +13,17 @@ app.debug = True
 
 db.init_app(app)
 DEBUG_TOOLBAR_ENABLED = True
+
 #toolbar = DebugToolbarExtension(app)
+
 from myfilters import *
+
 app.add_url_rule('/', view_func=index)
 app.add_url_rule('/detail/<id>', view_func=detail)
 app.add_url_rule('/vector/<vector_title>', view_func=vector)
 app.add_url_rule('/update/<id>', view_func=update, methods=['POST'])
 app.add_url_rule('/delete/<id>', view_func=delete, methods=['POST'])
-
+app.add_url_rule('/upload', view_func=upload, methods=['POST'])
 
 
 if __name__ == "__main__":
--- views.py
+++ views.py
@@ -1,22 +1,33 @@
 import flask
+import os
+#import sys
+import tempfile
 from flask import request, jsonify
 from models import Apk, FastApkVectors
+import subprocess
+#from subprocess import PIPE
+#import AndroBugs_MassiveAnalysis
 
+ANDROBUGS_DIR = "Your Path to AndroBugs_Framework directory"
+ANDROBUGS_EXE = "AndroBugs_MassiveAnalysis.py"
+ANALYZE_ENGINE_BUILD = 1
+ANALYZE_TAG = "AB-TAG"
+OUTPUT_DIR = "/tmp/out"
 
 def index():
-    '''
+    """
     Current home page returns all analyzed APKs
     :return: all APKs
-    '''
+    """
     apks = Apk.objects.all()
     return flask.render_template('index.html', apks=apks)
 
 
 def update(id):
-    '''
+    """
     Updates the comment field from the index view
     :return: Success or failure
-    '''
+    """
 
     try:
         comment = request.form['comment']
@@ -29,10 +40,10 @@ def update(id):
 
 
 def delete(id):
-    '''
+    """
     Delete an existing apk record
     :return: Success or failure
-    '''
+    """
     try:
         Apk.objects(id = id).delete()
         return jsonify(message="Delete successful.")
@@ -41,10 +52,10 @@ def delete(id):
 
     
 def detail(id):
-    '''
+    """
     Displays apks with same name
     :return: the view template
-    '''
+    """
     apk = Apk.objects.get(id=id)
     print "***********************"
     print apk.details
@@ -52,11 +63,11 @@ def detail(id):
 
 
 def level(name):
-    '''
+    """
     Displays apks containing vulnerabilities of a certain level
     :param name: package name of apk clicked
     :return: level template
-    '''
+    """
     apks = Apk.objects(package_name=name)
     return flask.render_template('level.html', apks=apks, package_name=name)
 
@@ -69,3 +80,34 @@ def vector(vector_title):
         if(vector_title in getattr(apk, "details").keys()):
             apks.append(apk)
     return flask.render_template('vector.html', apks=apks, vectors=vectors, vector_name=vector_title)
+
+def upload():
+    """
+    Upload an apk and process with the androbugs massive analyzer.
+    Customize your path to uploads and androbugs executable path and arguments
+    :return: success or failure
+    """
+    if request.method == 'POST':
+        f = request.files.get('file')
+        # Create tmp directory to not evaluate an ever-growing directory
+        upload_dir = tempfile.mkdtemp()
+        filepath = os.path.join(upload_dir, f.filename)
+        f.save(filepath)
+
+        # Execute androbugs
+        """
+        You could import or run subprocess,  Import example below
+    
+        sys.argv += ['-d' + UPLOAD_DIR, '-b' + str(ANALYZE_ENGINE_BUILD), '-t' + str(ANALYZE_TAG),
+                     '-o' + OUTPUT_DIR]
+        AndroBugs_MassiveAnalysis.main()
+        """
+
+        try:
+            process = subprocess.call(['python', ANDROBUGS_EXE, '-d', upload_dir, '-b', str(ANALYZE_ENGINE_BUILD), '-t',
+                                       str(ANALYZE_TAG), '-o', OUTPUT_DIR], cwd=ANDROBUGS_DIR)
+        except Exception as e:
+            return jsonify(message=e.message), 500
+
+
+        return jsonify(success=1)
